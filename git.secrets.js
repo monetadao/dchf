49dae617eff61cb6772a0e26da6ebc91d519c9d9
Matheus Kuck <matheuskuck@gmail.com>
2022-08-24 18:47:13 -0300

    Update readme and config templates

--- git.secrets.js
+++ git.secrets.js
@@ -1,3 +1,4 @@
+// Dev Values
 const secrets = {
   alchemyAPIKey: "2sDTn5oKXqo8Kzjw1XisAoqk_qUJtUk5",
   DEPLOYER_PRIVATEKEY: "0xbd6a50838e691b935deaa6a68c8ee3805c5a72d08e219deb9ae002a226fb632b",
---
+++ git.secrets.template.js
@@ -0,0 +1,13 @@
+// Dev Values
+const secrets = {
+  alchemyAPIKey: "",
+  DEPLOYER_PRIVATEKEY: "0x",
+  ACCOUNT2_PRIVATEKEY: "0xb",
+  alchemyAPIKeyRinkeby: "",
+  RINKEBY_DEPLOYER_PRIVATEKEY: "0x",
+  ETHERSCAN_API_KEY: "",
+}
+
+module.exports = {
+  secrets
+}
