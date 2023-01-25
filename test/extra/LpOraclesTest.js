53af6571e2db8a4fc37a58022103d15ba6720ecb
borus-dev <smkbcross@gmail.com>
2023-01-25 20:18:41 +0100

    test: added test to test opening troves with new oracles and lpToken

--- test/extra/LpOraclesTest.js
+++ test/extra/LpOraclesTest.js
@@ -9,11 +9,14 @@ const dec = th.dec
 const ZERO_ADDRESS = th.ZERO_ADDRESS
 
 const { abi: BOpABI } = require('../../artifacts/contracts/BorrowerOperations.sol/BorrowerOperations.json')
+const { abi: ERC20ABI } = require('../../artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json')
 
 const BORROWER_OPERATIONS = '0x9eB2Ce1be2DD6947e4f5Aabe33106f48861DFD74'
 const DFRANC_PARAMS = '0x6F9990B242873d7396511f2630412A3fcEcacc42'
 
 const MULTISIG = '0x83737EAe72ba7597b36494D723fbF58cAfee8A69'
+
+const LP_TOKEN_HOLDER = '0x005fb56Fe0401a4017e6f046272dA922BBf8dF06' // frax lpToken holder
 
 const GV_FRAX = '0xF437C8cEa5Bb0d8C10Bb9c012fb4a765663942f1'
 const CHAINLINK_USD_CHF = '0x449d117117838ffa61263b61da6301aa2a88b13a'
@@ -55,6 +58,22 @@ const deployParams = [
 const ethParams = [_chfFeed, _chfFeedTimeout, CHAINLINK_ETHUSD, _timeout]
 const btcParams = [_chfFeed, _chfFeedTimeout, CHAINLINK_BTCUSD, _timeout]
 
+async function fundAccount(account, holder, lpTokenAddress) {
+  await Promise.all([
+    hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [holder],
+    }),
+    hre.network.provider.send('hardhat_setBalance', [holder, '0x33B2E3C9FD0803CE8000000']),
+  ])
+
+  const signer = await hre.ethers.getSigner(holder)
+  const lpToken = new ethers.Contract(lpTokenAddress, ERC20ABI, signer)
+
+  await lpToken.approve(holder, hre.ethers.utils.parseUnits('10000', 18))
+  await lpToken.transferFrom(holder, account, hre.ethers.utils.parseUnits('10000', 18))
+}
+
 describe('Oracle', function () {
   let GVOracle
   let PriceFeed
@@ -82,15 +101,15 @@ describe('Oracle', function () {
 
   describe('Add asset to price feed and fetch price', function () {
     it('Can add a new Chainlink3PoolPairedLpOracle asset and fetch the price', async function () {
-      await PriceFeed.addOracle(GV_FRAX, GVOracle.address)
+      await PriceFeed.addOracle(_gvToken, GVOracle.address)
 
-      const registeredOracle = await PriceFeed.registeredOracles(GV_FRAX)
+      const registeredOracle = await PriceFeed.registeredOracles(_gvToken)
       expect(registeredOracle).to.be.eq(GVOracle.address)
 
-      const priceDirect = await PriceFeed.getDirectPrice(GV_FRAX)
+      const priceDirect = await PriceFeed.getDirectPrice(_gvToken)
       console.log('PriceDirect GVFrax3Crv in CHF:', priceDirect.toString())
 
-      const priceFetch = await PriceFeed.callStatic.fetchPrice(GV_FRAX)
+      const priceFetch = await PriceFeed.callStatic.fetchPrice(_gvToken)
       console.log('PriceFetch GVFrax3Crv in CHF:', priceFetch.toString())
 
       expect(priceDirect.toString()).to.be.eq(priceFetch.toString())
@@ -152,7 +171,7 @@ describe('Oracle', function () {
   })
 
   describe('Gas testing of current and new PriceFeeds', function () {
-    it('Opens a Trove with 3 accounts but different priceFeeds, gas reporting purposes', async function () {
+    it('Opens a Trove with 4 accounts but different priceFeeds, gas reporting purposes', async function () {
       await PriceFeed.addOracle(ZERO_ADDRESS, ChainlinkOracleETH.address)
 
       // new instance of the current mainnet deployed BorrowerOperations
@@ -172,7 +191,7 @@ describe('Oracle', function () {
         { value: coll }
       )
       const txIData = await txI.wait()
-      console.log('gasUsed I old PriceFeed first openTrove:', txIData.cumulativeGasUsed.toNumber()) // 560559
+      console.log('gasUsed I old PriceFeed 1st openTrove:', txIData.cumulativeGasUsed.toNumber()) // 560559
 
       const txII = await BorrowerOperations.connect(Account2).openTrove(
         ZERO_ADDRESS,
@@ -184,7 +203,7 @@ describe('Oracle', function () {
         { value: coll }
       )
       const txIIData = await txII.wait()
-      console.log('gasUsed II old PriceFeed second openTrove:', txIIData.cumulativeGasUsed.toNumber()) // 542855
+      console.log('gasUsed II old PriceFeed 2nd openTrove:', txIIData.cumulativeGasUsed.toNumber()) // 542855
 
       // new instance of the current mainnet deployed DfrancParameters
       const DfrancParametersFactory = await ethers.getContractFactory('DfrancParameters')
@@ -213,7 +232,7 @@ describe('Oracle', function () {
         { value: coll }
       )
       const txIIIData = await txIII.wait()
-      console.log('gasUsed III new PriceFeed first openTrove:', txIIIData.cumulativeGasUsed.toNumber()) // 508591
+      console.log('gasUsed III new PriceFeed 1st openTrove:', txIIIData.cumulativeGasUsed.toNumber()) // 508591
 
       const txIV = await BorrowerOperations.connect(Account4).openTrove(
         ZERO_ADDRESS,
@@ -225,7 +244,91 @@ describe('Oracle', function () {
         { value: coll }
       )
       const txIVData = await txIV.wait()
-      console.log('gasUsed IV new PriceFeed second openTrove:', txIVData.cumulativeGasUsed.toNumber()) // 508591
+      console.log('gasUsed IV new PriceFeed 2nd openTrove:', txIVData.cumulativeGasUsed.toNumber()) // 508591
+    })
+
+    it('Opens new Troves with new asset and new priceFeed, gas reporting purposes', async function () {
+      await PriceFeed.addOracle(_lpToken, ChainlinkOracleETH.address)
+
+      // new instance of the current mainnet deployed BorrowerOperations
+      const BorrowerOperationsFactory = await ethers.getContractFactory('BorrowerOperations')
+      const BorrowerOperations = BorrowerOperationsFactory.attach(BORROWER_OPERATIONS)
+
+      // new instance of the current mainnet deployed DfrancParameters
+      const DfrancParametersFactory = await ethers.getContractFactory('DfrancParameters')
+      const DfrancParams = DfrancParametersFactory.attach(DFRANC_PARAMS)
+
+      await network.provider.request({
+        method: 'hardhat_impersonateAccount',
+        params: [MULTISIG],
+      })
+      const multiSig = ethers.provider.getUncheckedSigner(MULTISIG)
+
+      // fund the multisig account to be able to process tx
+      const forceETH = await ethers.getContractFactory('ForceETH')
+      await forceETH.deploy(MULTISIG, { value: hre.ethers.utils.parseUnits('10', 18) })
+
+      // set the new priceFeed contract in dfrancParameters
+      await DfrancParams.connect(multiSig).setPriceFeed(PriceFeed.address)
+      // we add the new asset by setting as default in dfrancParameters
+      await DfrancParams.connect(multiSig).setAsDefault(_lpToken)
+
+      const amount = hre.ethers.utils.parseUnits('10000', 18)
+      const minDebt = hre.ethers.utils.parseUnits('2500', 18)
+
+      // new instance of the current mainnet deployed curve lpToken
+      const lpTokenFactory = await ethers.getContractFactory('ERC20')
+      const lpToken = lpTokenFactory.attach(_lpToken)
+
+      // fund with lpTokens account1
+      await fundAccount(Account1.address, LP_TOKEN_HOLDER, _lpToken)
+
+      // allow the borrower operations as spender
+      await lpToken.connect(Account1).approve(BorrowerOperations.address, amount)
+
+      const txI = await BorrowerOperations.connect(Account1).openTrove(
+        _lpToken,
+        amount,
+        th._100pct,
+        minDebt,
+        ZERO_ADDRESS,
+        ZERO_ADDRESS
+      )
+      const txIData = await txI.wait()
+      console.log('gasUsed I new PriceFeed lpToken 1st openTrove:', txIData.cumulativeGasUsed.toNumber()) // 694456
+
+      // fund with lpTokens account2
+      await fundAccount(Account2.address, LP_TOKEN_HOLDER, _lpToken)
+
+      // allow the borrower operations as spender
+      await lpToken.connect(Account2).approve(BorrowerOperations.address, amount)
+
+      const txII = await BorrowerOperations.connect(Account2).openTrove(
+        _lpToken,
+        amount,
+        th._100pct,
+        minDebt,
+        ZERO_ADDRESS,
+        ZERO_ADDRESS
+      )
+      const txIIData = await txII.wait()
+      console.log('gasUsed II new PriceFeed lpToken 2nd openTrove:', txIIData.cumulativeGasUsed.toNumber()) // 521359
+
+      // fund with lpTokens account3
+      await fundAccount(Account3.address, LP_TOKEN_HOLDER, _lpToken)
+
+      // allow the borrower operations as spender
+      await lpToken.connect(Account3).approve(BorrowerOperations.address, amount)
+      const txIII = await BorrowerOperations.connect(Account3).openTrove(
+        _lpToken,
+        amount,
+        th._100pct,
+        minDebt,
+        ZERO_ADDRESS,
+        ZERO_ADDRESS
+      )
+      const txIIIData = await txIII.wait()
+      console.log('gasUsed III new PriceFeed lpToken 3rd openTrove:', txIIIData.cumulativeGasUsed.toNumber()) // 521359
     })
   })
 
