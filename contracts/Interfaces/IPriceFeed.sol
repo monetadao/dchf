3002f89afe2791ad6e4c7aa787a068d9888a5823
borus-dev <smkbcross@gmail.com>
2023-07-25 23:24:16 +0200

    feat: added support in PriceFeed for additional oracles

--- contracts/Interfaces/IPriceFeed.sol
+++ contracts/Interfaces/IPriceFeed.sol
@@ -1,42 +1,32 @@
 // SPDX-License-Identifier: MIT
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 pragma solidity ^0.8.14;
 
 interface IPriceFeed {
-	struct ChainlinkResponse {
-		uint80 roundId;
+	struct OracleResponse {
 		int256 answer;
 		uint256 timestamp;
-		bool success;
 		uint8 decimals;
 	}
 
 	struct RegisterOracle {
-		AggregatorV3Interface chainLinkOracle;
+		IOracle priceOracle;
 		AggregatorV3Interface chainLinkIndex;
 		bool isRegistered;
 	}
 
-	enum Status {
-		chainlinkWorking,
-		chainlinkUntrusted
-	}
-
 	// --- Events ---
-	event PriceFeedStatusChanged(Status newStatus);
 	event LastGoodPriceUpdated(address indexed token, uint256 _lastGoodPrice);
 	event LastGoodIndexUpdated(address indexed token, uint256 _lastGoodIndex);
-	event RegisteredNewOracle(
-		address token,
-		address chainLinkAggregator,
-		address chianLinkIndex
-	);
+	event RegisteredNewOracle(address token, address oracle, address chainLinkIndex);
+	event newIntervalSet(uint256 newInterval, uint256 oldInterval);
 
 	// --- Function ---
 	function addOracle(
 		address _token,
-		address _chainlinkOracle,
+		address _oracle,
 		address _chainlinkIndexOracle
 	) external;
 
@@ -44,3 +34,25 @@ interface IPriceFeed {
 
 	function getDirectPrice(address _asset) external returns (uint256);
 }
+
+interface IOracle {
+	function decimals() external view returns (uint8);
+
+	function description() external view returns (string memory);
+
+	function version() external view returns (uint256);
+
+	function latestAnswer() external view returns (int256 answer, uint256 updatedAt);
+
+	function latestRoundData()
+		external
+		view
+		returns (
+			uint80 roundId,
+			int256 answer,
+			uint256 startedAt,
+			uint256 updatedAt,
+			uint80 answeredInRound
+		);
+}
+
